b67487829e47a08b9b77bc7a169821f39f119482
Krzysztof Skrzynecki <skrzynecki.krzysztof@gmail.com>
2017-05-07 18:37:28 +0100

    Added first basic index html template

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'nickname': 'Bob'}
+    return render_template("index.html",
+                           title="Home",
+                           user=user)
